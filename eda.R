f9c9a8f764ee4bf1c0b87f4a48cf029169a125c2
krscmbs <krisrcoombes@gmail.com>
2023-01-08 23:36:16 +0000

    eda

--- eda.R
+++ eda.R
@@ -28,5 +28,27 @@ top_tracks_features <- rbind(features, top_tracks_features)
 
 }
 
+# Join
 top_tracks <- top_tracks %>% 
   left_join(top_tracks_features, by = 'id')
+
+# Unnest artist name
+top_tracks$artist_name <- purrr::map_chr(top_tracks$album.artists, function(x) x$name[1])
+
+#### Descriptives ####
+
+top_tracks_summary <- top_tracks %>% 
+  summarise(distinct_artists = n_distinct(artist_name),
+            earliest_release = min(album.release_date, na.rm = TRUE),
+            latest_release = max(album.release_date, na.rm = TRUE),
+            avg_length = mean(duration_ms.x/60000, na.rm = TRUE),
+            avg_valence = mean(valence, na.rm = TRUE),
+            avg_danceability = mean(danceability, na.rm = TRUE),
+            avg_energy = mean(energy, na.rm = TRUE),
+            avg_loudness = mean(loudness, na.rm = TRUE),
+            avg_speechiness = mean(speechiness, na.rm = TRUE),
+            avg_instrumentalness = mean(instrumentalness, na.rm = TRUE),
+            avg_liveness = mean(liveness, na.rm = TRUE),
+            avg_tempo = mean(tempo, na.rm = TRUE),
+            avg_acousticness = mean(acousticness, na.rm = TRUE),
+            avg_popularity = mean(popularity, na.rm = TRUE))
